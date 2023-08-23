306cd9beb4f7c05687a61b84245be30910061cf2
MariaHeloisa300512 <119361001+MariaHeloisa300512@users.noreply.github.com>
2023-08-23 17:12:20 -0300

    Add files via upload

---
+++ C126-main/C126-main/main.js
@@ -0,0 +1,19 @@
+var pulsoEsquerdo = 0;
+var pulsoDireito = 0;
+
+var pulsoDireitoX = 0;
+var pulsoDireitoY = 0;
+
+var pulsoEsquerdoX = 0;
+var pulsoEsquerdoY = 0;
+var canvas;
+
+function preload (){
+
+}
+function setup(){
+
+}
+function draw(){
+    canvas = createCanvas(300,300);
+}
